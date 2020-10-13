e779025e26cdecad13cd2a5514e10ccecb910fae
soham1111 <roysoham0007@gmail.com>
2020-10-13 18:52:13 +0530

    gitbash bawdwa

--- Chain.js
+++ Chain.js
@@ -1,20 +1,26 @@
 class Chain{
-    constructor(bodyA, bodyB){
+    constructor(bodyA, pointB){
         var options = {
             bodyA: bodyA,
-            bodyB: bodyB,
-            stiffness: 0.04,
+            pointB: pointB,
+            stiffness: 0.02,
             length: 10
         }
+        this.pointB= pointB;
         this.chain = Constraint.create(options);
         World.add(world, this.chain);
     }
 
     display(){
+        if(this.chain.bodyA){
         var pointA = this.chain.bodyA.position;
-        var pointB = this.chain.bodyB.position;
+        var pointB = this.pointB;
         strokeWeight(4);
         line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+    }
+    fly(){
+        this.chain.bodyA=null
     }
     
 }
